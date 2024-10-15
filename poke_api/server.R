5903a4a4acc25b56c0579540c9a71b9ab2f26f0b
Christian <christianbarrios@ufm.edu>
2024-10-14 18:22:44 -0600

    Laboratorio 4 Resuelto
    Faltan ccambios pequenios

--- poke_api/server.R
+++ poke_api/server.R
@@ -1,28 +1,67 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
+# server.R
 
 library(shiny)
+library(httr)
+library(jsonlite)
+library(dplyr)
+library(plotly)
+library(DT)
 
-# Define server logic required to draw a histogram
-function(input, output, session) {
-
-    output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-
-    })
-
+# Definición del Servidor
+server <- function(input, output, session) {
+  
+  # Función para obtener datos de un Pokémon desde la API
+  getPokemonData <- function(pokemonName) {
+    url <- paste0("https://pokeapi.co/api/v2/pokemon/", tolower(pokemonName))
+    response <- GET(url)
+    
+    if (status_code(response) == 200) {
+      data <- content(response, as = "parsed", type = "application/json")
+      stats <- data.frame(
+        Estadística = sapply(data$stats, function(stat) stat$stat$name),
+        Valor = sapply(data$stats, function(stat) stat$base_stat)
+      )
+      return(stats)
+    } else {
+      return(NULL)
+    }
+  }
+  
+  # Evento para obtener y mostrar datos del Pokémon
+  observeEvent(input$fetch, {
+    stats <- getPokemonData(input$pokemon)
+    if (!is.null(stats)) {
+      output$pokemonPlot <- renderPlotly({
+        plot_ly(stats, x = ~Estadística, y = ~Valor, type = 'bar', 
+                marker = list(color = 'rgba(55, 128, 191, 0.6)')) %>%
+          layout(title = paste("Estadísticas de", input$pokemon))
+      })
+      
+      output$pokemonTable <- renderDT({
+        datatable(stats, options = list(pageLength = 5))
+      })
+    } else {
+      showNotification("Pokémon no encontrado. Intenta con otro nombre.", type = "error")
+    }
+  })
+  
+  # Evento para comparar dos Pokémon
+  observeEvent(input$compare, {
+    stats1 <- getPokemonData(input$pokemon1)
+    stats2 <- getPokemonData(input$pokemon2)
+    
+    if (!is.null(stats1) && !is.null(stats2)) {
+      # Utilizar sufijos estáticos
+      combinedStats <- merge(stats1, stats2, by = "Estadística", suffixes = c('_1', '_2'))
+      
+      output$comparisonPlot <- renderPlotly({
+        plot_ly(combinedStats, x = ~Estadística) %>%
+          add_trace(y = ~Valor_1, name = input$pokemon1, type = 'bar') %>%
+          add_trace(y = ~Valor_2, name = input$pokemon2, type = 'bar') %>%
+          layout(barmode = 'group', title = paste("Comparación entre", input$pokemon1, "y", input$pokemon2))
+      })
+    } else {
+      showNotification("Uno o ambos Pokémon no fueron encontrados. Intenta con otros nombres.", type = "error")
+    }
+  })
 }
--- poke_api/ui.R
+++ poke_api/ui.R
@@ -1,33 +1,52 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
+# ui.R
 
 library(shiny)
+library(shinydashboard)
+library(shinythemes)
+library(plotly)
+library(DT)
+library(shinycssloaders)
 
-# Define UI for application that draws a histogram
-fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
-        )
+# Definición de la UI
+ui <- dashboardPage(
+  dashboardHeader(title = "Análisis de Pokémon"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Inicio", tabName = "inicio", icon = icon("home")),
+      menuItem("Gráficos", tabName = "graficos", icon = icon("chart-bar")),
+      menuItem("Comparación", tabName = "comparacion", icon = icon("balance-scale")),
+      menuItem("Datos", tabName = "datos", icon = icon("table"))
+    ),
+    textInput('pokemon', 'Ingresa el nombre de un Pokémon', value = 'pikachu'),
+    actionButton('fetch', 'Consultar Pokémon')
+  ),
+  dashboardBody(
+    shinythemes::themeSelector(), 
+    tabItems(
+      # Pestaña de Inicio
+      tabItem(tabName = "inicio",
+              h2("Bienvenido al Análisis de Pokémon"),
+              p("Utiliza esta aplicación para explorar y comparar estadísticas de distintos Pokémon.")),
+      
+      # Pestaña de Gráficos
+      tabItem(tabName = "graficos",
+              h2("Gráficos Interactivos"),
+              plotlyOutput('pokemonPlot') %>% withSpinner(color="#0dc5c1")),
+      
+      # Pestaña de Comparación
+      tabItem(tabName = "comparacion",
+              h2("Comparación de Pokémon"),
+              fluidRow(
+                column(6, textInput('pokemon1', 'Pokémon 1', value = 'bulbasaur')),
+                column(6, textInput('pokemon2', 'Pokémon 2', value = 'charmander'))
+              ),
+              actionButton('compare', 'Comparar'),
+              plotlyOutput('comparisonPlot') %>% withSpinner(color="#0dc5c1")),
+      
+      # Pestaña de Datos
+      tabItem(tabName = "datos",
+              h2("Datos Interactivos"),
+              DTOutput('pokemonTable') %>% withSpinner(color="#0dc5c1"))
     )
+  )
 )
